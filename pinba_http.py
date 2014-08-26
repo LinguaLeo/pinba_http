c4a654736b2e8e2dfa419221d65d580e8a63adcc
justgoodman <e.pak@lingualeo.com>
2014-08-26 17:24:14 +0400

    remove constant pumba host and port to wsga_params

--- pinba_http.py
+++ pinba_http.py
@@ -3,12 +3,15 @@
 from cgi import parse_qs
 from socket import socket, gethostname, AF_INET, SOCK_DGRAM
 from sys import argv
+
 import re
+import logging
 import pinba_pb2
 
 VERSION = 1.1
-PINBA_HOST = '127.0.0.1'
-PINBA_PORT = 30002
+
+# For work we need set PINBA_HOST,PINBA_PORT value in file uswgi_params,this file located in folder nginx 
+
 TIMER_MAX = 10*60
 
 udpsock = socket(AF_INET, SOCK_DGRAM)
@@ -17,7 +20,7 @@ hostname = gethostname()
 class InvalidTimer(Exception):
     pass
 
-def pinba(server_name, tracker, timer, tags):
+def pinba(server_name, tracker, timer, tags,pinba_host,pinba_port):
     """
     Send a message to Pinba.
 
@@ -25,7 +28,10 @@ def pinba(server_name, tracker, timer, tags):
     :param tracker:     tracker name
     :param timer:       timer value in seconds
     :param tags:        dictionary of tags
-    """
+    :param pinba_host:  pinba host
+    :param pinba_port:  pinba port
+    """    
+
     if timer < 0 or timer > TIMER_MAX:
         raise InvalidTimer()
 
@@ -67,7 +73,7 @@ def pinba(server_name, tracker, timer, tags):
         msg.dictionary.extend(dictionary);
 
     # Send message to Pinba server
-    udpsock.sendto(msg.SerializeToString(), (PINBA_HOST, PINBA_PORT))
+    udpsock.sendto(msg.SerializeToString(), (pinba_host, pinba_port))
 
 def generic(prefix, environ):
     """
@@ -82,56 +88,32 @@ def generic(prefix, environ):
         timer = float(tags.pop('t')[0])
     except KeyError:
         timer = 0.0
-    pinba(environ['HTTP_HOST'], tracker, timer, tags)
-
-class Boomerang(object):
+    pinba_host = environ['PINBA_HOST']
+    pinba_port = environ['PINBA_PORT'] 
     """
-    Handler for Yahoo Boomerang.
-
-    https://github.com/yahoo/boomerang
-
-    Parameters matching `.?t_` are considered as timestamps, except
-    `t_resp`, `t_page` and `t_done`. Timestamps are transformed into
-    timers by making the difference with `nt_nav_st`.
-    """
-
-    def __init__(self):
-        self.timestamps_re = re.compile("^.?t_")
-
-    def is_timer(self, name):
-        """Is it a timer in milliseconds?"""
-        return name in ["t_resp", "t_page", "t_done"]
-
-    def is_timestamp(self, name):
-        """Is it a timestamp in milliseconds?"""
-        if self.timestamps_re.match(name) and not self.is_timer(name):
-            return True
-        return False
-
-    def __call__(self, prefix, environ):
-        tags = parse_qs(environ['QUERY_STRING'])
-        try:
-            # Start point for other timestamps
-            start = int(tags.pop("nt_nav_st")[0])
-        except KeyError:
-            raise InvalidTimer
-        timers = {}
-        for t in tags.keys()[:]:
-            if self.is_timer(t):
-                timers[t] = int(tags.pop(t)[0])
-            elif self.is_timestamp(t):
-                val = int(tags.pop(t)[0]) - start
-                if val < 0:
-                    continue
-                timers[t] = val
-        for t in timers:
-            pinba(environ['HTTP_HOST'], "boomerang.%s" % t, timers[t]/1000., tags)
+    logging.basicConfig(filename='pinba-http.log',level=logging.INFO)
+    str_params = 'Params :' + '\n'
+    str_params = str_params + 'PINBA_HOST: ' + str(pinba_host) + '\n'
+    str_params = str_params + 'PINBA_PORT: ' + str(pinba_port) + '\n'
+    str_params = str_params + 'server_name: ' + environ['HTTP_HOST'] + '\n'; 
+    str_params = str_params + 'tracker:' + tracker + '\n'
+    str_params = str_params + 'timer: ' + str(timer) + '\n' 
+    str_params = str_params + 'tags: ' + get_array_string(tags) + '\n' 
+    logging.info(str_params)
+    """  
+    pinba(environ['HTTP_HOST'], tracker, timer, tags,pinba_host,pinba_port)
 
 # Simple routing
 handlers = {
-    "/track/": generic,
-    "/track-boomerang/": Boomerang()
+    "/track/": generic
 }
+"""
+def get_array_string(array):
+  res = ''
+  for key in array:
+     res = res + key + str(array[key]) + ','
+  return res   
+"""
 
 def app(environ, start_response):
     for h in handlers:
@@ -141,7 +123,7 @@ def app(environ, start_response):
             except InvalidTimer:
                 start_response('400 Invalid Timer', [('Content-Length', 0)])
                 return ['']
-            start_response('200 OK', [('Content-Length', 0)])
+            start_response('200 OK', [('Content-Length', '0')])
             return ['']
-    start_response('404 Not Found', [('Content-Length', 0)])
+    start_response('404 Not Found', [('Content-Length', '0')])
     return ['']
